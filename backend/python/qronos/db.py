5b85e5c2f1b978e37c6941cc75671755f9c0761d
Roy Russo <royrusso@gmail.com>
2024-06-25 16:45:34 -0400

    ---

--- backend/python/qronos/db.py
+++ backend/python/qronos/db.py
@@ -5,6 +5,23 @@ from sqlalchemy.engine import Engine
 from qronos.settings import SETTINGS, Settings, Env
 
 
+# class User(SQLModel, table=True):
+#     id: UUID = Field(default=None, primary_key=True)
+#     username: str
+#     password: str
+#     email: str
+#     updated_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+#     created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+
+
+# class SystemSettings(SQLModel, table=True):
+#     id: Column(Integer, Sequence('setting_id_seq'), primary_key=True)
+#     key: str
+#     value: str
+#     updated_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+#     created_at: datetime = Field(default_factory=lambda: datetime.now(tz=timezone.utc))
+
+
 class RunHistory(SQLModel, table=True):
     id: UUID = Field(default=None, primary_key=True)
     stdout: str = Field(default=None, nullable=True)
--- frontend/src/types/qronos.ts
+++ frontend/src/types/qronos.ts
@@ -3,6 +3,7 @@ import * as t from "io-ts";
 export const User = t.type({
   id: t.number,
   username: t.string,
+  oassword: t.string,
   email: t.string,
   created_at: t.string,
   updated_at: t.string,
@@ -21,12 +22,12 @@ export const SystemSettings = t.type({
 export type SystemSettings = t.TypeOf<typeof SystemSettings>;
 
 export const SystemStatus = t.type({
-    id: t.number,
-    name: t.string,
-    value: t.string,
-    created_at: t.string,
-    updated_at: t.string,
-    });
+  id: t.number,
+  name: t.string,
+  value: t.string,
+  created_at: t.string,
+  updated_at: t.string,
+});
 
 export type SystemStatus = t.TypeOf<typeof SystemStatus>;
 
@@ -35,34 +36,34 @@ export type SystemStatus = t.TypeOf<typeof SystemStatus>;
  * code_id will be used a an FK.
  */
 export const Code = t.type({
-    code_id: t.string,
-    content: t.string,
-    version: t.number,
-    user: User,
-    created_at: t.string,
-    updated_at: t.string,
-    });
+  code_id: t.string,
+  content: t.string,
+  version: t.number,
+  user: User,
+  created_at: t.string,
+  updated_at: t.string,
+});
 
 export type Code = t.TypeOf<typeof Code>;
 
 export const Script = t.type({
-    id: t.number,
-    name: t.string,
-    description: t.string,
-    code: Code, // most recent code. DB should just have a code_id that FKs to the Code table.
-    type: t.string,
-    created_at: t.string,
-    updated_at: t.string,
-    });
+  id: t.number,
+  name: t.string,
+  description: t.string,
+  code: Code, // most recent code. DB should just have a code_id that FKs to the Code table.
+  type: t.string,
+  created_at: t.string,
+  updated_at: t.string,
+});
 
 export type Script = t.TypeOf<typeof Script>;
 
 export const Job = t.type({
-    id: t.number,
-    script: Script,
-    status: t.string,
-    created_at: t.string,
-    updated_at: t.string,
-    });
+  id: t.number,
+  script: Script,
+  status: t.string,
+  created_at: t.string,
+  updated_at: t.string,
+});
 
 export type Job = t.TypeOf<typeof Job>;

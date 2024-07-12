a700c92fdc56c32dcbf69d02bb6d3ada24661e69
Roy Russo <royrusso@gmail.com>
2024-07-12 18:02:08 -0400

    -- added react icons

--- frontend/src/components/pages/create_scripts.tsx
+++ frontend/src/components/pages/create_scripts.tsx
@@ -1,5 +1,7 @@
 import { generateSlug } from "random-word-slugs";
 import { useEffect } from "react";
+import { IconContext } from "react-icons";
+import { VscOpenPreview, VscSave } from "react-icons/vsc";
 import { QronosEditor } from "../core/qronos_editor";
 
 const CreateScript = () => {
@@ -23,210 +25,105 @@ const CreateScript = () => {
     <>
       <div className="ml-64 mt-16 p-4">
         <h1>Create Script</h1>
-        <button className="btn">Button</button>
 
-        <form>
+        <form className="mt-5">
           <div className="grid gap-4 sm:grid-cols-3 sm:gap-6">
             <div className="sm:col-span-1">
               <input
                 type="text"
+                className="input input-bordered w-full max-w-xs"
                 name="script_name"
                 id="script_name"
                 onChange={() => {}} // TODO: check for uniqueness
-                className="rounded-lg"
                 required
               ></input>
             </div>
             <div className="sm:col-span-1">
-              <div className="flex items-center">
+              <div className="flex items-center justify-center">
                 <select
+                  className="select select-bordered w-full max-w-xs"
                   id="script_type"
                   required
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5
-                   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 >
                   <option value="RUNNABLE">Runnable Script</option>
                   <option value="API">HTTP API</option>
                 </select>
-                <button
-                  data-popover-target="popover-description"
-                  data-popover-placement="right"
-                  type="button"
-                >
-                  <svg
-                    className="w-4 h-4 ms-1 text-gray-400 hover:text-gray-500"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
+                <div className="dropdown dropdown-end">
+                  <div
+                    className="tooltip"
+                    data-tip="Click for Description of Script Types..."
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
-                <div
-                  data-popover
-                  id="popover-description"
-                  role="tooltip"
-                  className="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800"
-                >
-                  <div className="px-3 py-2">
-                    <h5 className="font-semibold text-gray-900 dark:text-white">
-                      Runnable Script
-                    </h5>
-                    <p>
-                      A script that is triggered on a cron-like schedule or
-                      executed on-demand.
-                    </p>
-                    <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
-
-                    <h5 className="font-semibold text-gray-900 dark:text-white">
-                      HTTP API
-                    </h5>
-                    <p>Code that will listen and respond to HTTP requests.</p>
+                    <div
+                      tabIndex={0}
+                      role="button"
+                      className="btn btn-circle btn-ghost btn-xs ml-3"
+                    >
+                      <svg
+                        className="w-5 h-5"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                      >
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                        />
+                      </svg>
+                    </div>
                   </div>
-                  <div data-popper-arrow></div>
+                  <div
+                    tabIndex={0}
+                    className="card compact dropdown-content bg-base-100 rounded-box z-[1] w-64 shadow"
+                  >
+                    <div tabIndex={0} className="card-body">
+                      <h3 className="card-title">Script Types</h3>
+                      <div className="px-3">
+                        <h5>Runnable Script</h5>
+                        <p>
+                          A script that is triggered on a cron-like schedule or
+                          executed on-demand.
+                        </p>
+                        <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
+
+                        <h5>HTTP API</h5>
+                        <p>
+                          A Script that will listen and respond to HTTP
+                          requests.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="sm:col-span-1">
-              <div
-                className="inline-flex rounded-md shadow-sm float-right"
-                role="group"
-              >
-                <button
-                  type="button"
-                  data-tooltip-target="tooltip-cron"
-                  className=" max-h-32 px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
-                >
-                  <svg
-                    className="w-6 h-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
+              <div className="join float-right mr-5">
+                <button className="btn join-item">
+                  <IconContext.Provider
+                    value={{ className: "react-icon-button" }}
                   >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M18.5 4h-13m13 16h-13M8 20v-3.333a2 2 0 0 1 .4-1.2L10 12.6a1 1 0 0 0 0-1.2L8.4 8.533a2 2 0 0 1-.4-1.2V4h8v3.333a2 2 0 0 1-.4 1.2L13.957 11.4a1 1 0 0 0 0 1.2l1.643 2.867a2 2 0 0 1 .4 1.2V20H8Z"
-                    />
-                  </svg>
+                    <VscOpenPreview />
+                  </IconContext.Provider>
+                  Preview
                 </button>
-                <div
-                  id="tooltip-cron"
-                  role="tooltip"
-                  className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
-                >
-                  Schedule this script to run.
-                  <div className="tooltip-arrow" data-popper-arrow></div>
-                </div>
+
                 <button
                   type="button"
-                  id="dropdownDefaultButton"
-                  data-dropdown-toggle="dropdownEdit"
-                  className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200  hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-blue-500 dark:focus:text-white"
-                >
-                  <svg
-                    className="w-6 h-6 text-gray-800 dark:text-white"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M10 3v4a1 1 0 0 1-1 1H5m5 4-2 2 2 2m4-4 2 2-2 2m5-12v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1Z"
-                    />
-                  </svg>
-                </button>
-                <div
-                  id="dropdownEdit"
-                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-                >
-                  <ul
-                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdownDefaultButton"
-                  >
-                    <li>
-                      <a
-                        href="#"
-                        className="inline-flex  px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        <svg
-                          className="w-5 h-5 me-2 text-gray-800 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M9 8v3a1 1 0 0 1-1 1H5m11 4h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1v1m4 3v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-7.13a1 1 0 0 1 .24-.65L7.7 8.35A1 1 0 0 1 8.46 8H13a1 1 0 0 1 1 1Z"
-                          />
-                        </svg>
-                        Copy
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href="#"
-                        className="inline-flex  px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      >
-                        <svg
-                          className="w-5 h-5 me-2 text-gray-800 dark:text-white"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                        >
-                          <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z"
-                          />
-                        </svg>
-                        Delete
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <button
-                  type="button"
-                  className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 
-                  font-medium rounded-e-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                  className="btn btn-primary join-item"
                   onClick={handleSubmit}
                 >
-                  <svg
-                    className="w-3 h-3 me-2"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
+                  <IconContext.Provider
+                    value={{ className: "react-icon-button" }}
                   >
-                    <path d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
-                    <path d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
-                  </svg>
+                    <VscSave />
+                  </IconContext.Provider>
+                  {/* TODO: Save should be disabled unless dirty. */}
                   Save
                 </button>
               </div>
@@ -238,6 +135,33 @@ const CreateScript = () => {
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               ></label>
               <QronosEditor />
+            </div>
+          </div>
+
+          <div className="sm:col-span-1 mt-5">
+            <div className="join float-right mr-5">
+              <button className="btn join-item">
+                <IconContext.Provider
+                  value={{ className: "react-icon-button" }}
+                >
+                  <VscOpenPreview />
+                </IconContext.Provider>
+                Preview
+              </button>
+
+              <button
+                type="button"
+                className="btn btn-primary join-item"
+                onClick={handleSubmit}
+              >
+                <IconContext.Provider
+                  value={{ className: "react-icon-button" }}
+                >
+                  <VscSave />
+                </IconContext.Provider>
+                {/* TODO: Save should be disabled unless dirty. */}
+                Save
+              </button>
             </div>
           </div>
         </form>

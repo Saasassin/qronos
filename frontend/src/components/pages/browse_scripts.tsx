ff8cea4ccaa5cdb5009089a49cf0079b37e98da3
Roy Russo <royrusso@gmail.com>
2024-07-19 17:08:01 -0400

    - added react-table

--- frontend/src/components/pages/browse_scripts.tsx
+++ frontend/src/components/pages/browse_scripts.tsx
@@ -1,72 +1,97 @@
-import { useEffect, useState } from "react";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useEffect, useReducer, useState } from "react";
 import { FaRegChartBar, FaRegHourglass, FaRunning } from "react-icons/fa";
 import { IoCreateOutline } from "react-icons/io5";
 import { MdHttp } from "react-icons/md";
 import { RiDeleteBinLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
-import { Script, Scripts } from "../../types/qronos";
+import { Script } from "../../types/qronos";
 import { formatDateAndTime } from "../dateutils";
 import { deleteScript, fetchScripts } from "../services/Client";
 
-const ScriptTable = () => {
-  // = ({ scripts }: { scripts: Scripts }) => {
+const BrowseTable = ({ data }: { data: any }) => {
+  const rerender = useReducer(() => ({}), {})[1];
 
-  useEffect(() => {
-    fetchScripts().then((data) => {
-      setScripts(data);
-    });
-  }, []);
+  const columnHelper = createColumnHelper<Script>();
+  const columns = [
+    columnHelper.accessor("id", {
+      header: "",
+      cell: (row) => (
+        <>
+          <div className="join">
+            <div className="tooltip" data-tip="Edit Script">
+              <Link to={`/edit_script/${row.getValue()}`}>
+                <button className="btn btn-sm btn-neutral join-item">
+                  <IoCreateOutline />
+                </button>
+              </Link>
+            </div>
+            <div className="tooltip" data-tip="Run History">
+              <button className="btn btn-sm btn-neutral join-item">
+                <FaRegChartBar />
+              </button>{" "}
+            </div>
+            <div className="tooltip" data-tip="Schedule">
+              <button className="btn btn-sm btn-neutral join-item">
+                <FaRegHourglass />
+              </button>{" "}
+            </div>
+            <div className="tooltip" data-tip="Delete">
+              <button
+                className="btn btn-sm btn-neutral join-item"
+                onClick={() => showDeleteModal(row.getValue())}
+              >
+                <RiDeleteBinLine />
+              </button>{" "}
+            </div>
+          </div>
+        </>
+      ),
+    }),
+    columnHelper.accessor("script_name", {
+      header: "Name",
+      cell: (row) => row.getValue(),
+    }),
+    columnHelper.accessor("script_type", {
+      header: "Type",
+      cell: (row) => getScriptTypeIcon(row.getValue()),
+    }),
+    columnHelper.accessor("created_at", {
+      header: "Created At",
+      cell: (row) => formatDateAndTime(row.getValue()),
+    }),
+  ];
 
-  const [scripts, setScripts] = useState<Scripts>([]);
-  const [sortOrder, setSortOrder] = useState<"asc" | "desc">("desc");
-  const [sortField, setSortField] = useState<keyof Scripts[0]>("created_at");
-  const [selectedForDelete, setSelectedForDelete] = useState<Script>();
-
-  const handleSort = (field: keyof Scripts[0]) => {
-    setSortField(field);
-    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  type ColumnSort = {
+    id: string;
+    desc: boolean;
   };
+  type SortingState = ColumnSort[];
 
-  const sortedScripts = scripts.sort((a, b) => {
-    if (sortOrder === "asc") {
-      return a[sortField] && b[sortField]
-        ? a[sortField] > b[sortField]
-          ? 1
-          : -1
-        : 0;
-    } else {
-      return a[sortField] && b[sortField]
-        ? a[sortField] < b[sortField]
-          ? 1
-          : -1
-        : 0;
-    }
+  const [selectedForDelete, setSelectedForDelete] = useState<Script>();
+  const [sorting, setSorting] = useState<SortingState>([]); // can set initial sorting state here
+  const tableInstance = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    //getSortedRowModel: getSortedRowModel(), //client-side sorting
+    onSortingChange: setSorting,
+    state: {
+      sorting: [
+        {
+          id: "created_at",
+          desc: true,
+        },
+      ] as SortingState,
+    },
   });
 
-  const showDeleteModal = (script_id: string) => {
-    const deleteModal = document.getElementById(
-      "delete_modal"
-    ) as HTMLDialogElement;
-    deleteModal?.showModal();
-
-    // find the script in the list 'scripts' and set it as selected for delete
-    const script = scripts.find((script) => script.id === script_id);
-    if (script) {
-      setSelectedForDelete(script);
-    }
-  };
-
-  const confirmDeleteScript = (script_id: string) => {
-    const deleteModal = document.getElementById(
-      "delete_modal"
-    ) as HTMLDialogElement;
-    deleteModal?.close();
-
-    console.log("Deleting script with id: ", script_id);
-    deleteScript(script_id);
-
-    window.location.reload();
-  };
+  console.log(tableInstance.getState().sorting);
 
   const getScriptTypeIcon = (scriptType: string) => {
     if (scriptType === "RUNNABLE") {
@@ -87,84 +112,73 @@ const ScriptTable = () => {
       );
     }
   };
-  return (
-    <div className="overflow-x-auto mt-5">
-      <table className="table table-zebra  border border-dashed border-neutral border-collapse">
-        <thead>
-          <tr>
-            <th className="text-lg"></th>
-            <th className="text-lg">
-              <a href="#" onClick={() => handleSort("script_name")}>
-                Script Name
-              </a>
-            </th>
-            <th className="text-lg">
-              <a href="#" onClick={() => handleSort("script_type")}>
-                Type
-              </a>
-            </th>
-            <th className="text-lg">
-              <a href="#" onClick={() => handleSort("created_at")}>
-                Created
-              </a>
-            </th>
-            <th className="text-lg">Next Run</th>
-            <th className="text-lg">Last Run</th>
-            <th className="text-lg"></th>
-          </tr>
-        </thead>
-        <tbody>
-          {scripts.map((script) => (
-            <tr key={script.id} className="hover">
-              <td>
-                <div className="join">
-                  <div className="tooltip" data-tip="Edit Script">
-                    <Link to={`/edit_script/${script.id}`}>
-                      <button className="btn btn-sm btn-neutral join-item">
-                        <IoCreateOutline />
-                      </button>
-                    </Link>
-                  </div>
-                  <div className="tooltip" data-tip="Run History">
-                    <button className="btn btn-sm btn-neutral join-item">
-                      <FaRegChartBar />
-                    </button>{" "}
-                  </div>
-                  <div className="tooltip" data-tip="Schedule">
-                    <button className="btn btn-sm btn-neutral join-item">
-                      <FaRegHourglass />
-                    </button>{" "}
-                  </div>
-                  <div className="tooltip" data-tip="Delete">
-                    <button
-                      className="btn btn-sm btn-neutral join-item"
-                      onClick={() =>
-                        showDeleteModal(script.id ? script.id : "")
-                      }
-                    >
-                      <RiDeleteBinLine />
-                    </button>{" "}
-                  </div>
-                </div>
-              </td>
-              <td>{script.script_name}</td>
-              <td className="justify-center justify-items-center justify-self-center content-center items-center self-center place-content-center place-items-center place-self-center">
-                {getScriptTypeIcon(script.script_type)}
-              </td>
-              <td>{formatDateAndTime(script.created_at)}</td>
-              <td>... </td>
-              <td>...</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="join justify-center w-full mt-4">
-        <button className="join-item btn btn-active">1</button>
-        <button className="join-item btn ">2</button>
-        <button className="join-item btn ">3</button>
-        <button className="join-item btn ">4</button>
-      </div>
 
+  const showDeleteModal = (script_id: string = "") => {
+    const deleteModal = document.getElementById(
+      "delete_modal"
+    ) as HTMLDialogElement;
+    deleteModal?.showModal();
+
+    //find the script in the list 'scripts' and set it as selected for delete
+    const script = data.find(
+      (script: { id: string }) => script.id === script_id
+    );
+    if (script) {
+      setSelectedForDelete(script);
+    }
+  };
+
+  const confirmDeleteScript = (script_id: string) => {
+    const deleteModal = document.getElementById(
+      "delete_modal"
+    ) as HTMLDialogElement;
+    deleteModal?.close();
+
+    console.log("Deleting script with id: ", script_id);
+    deleteScript(script_id);
+
+    window.location.reload();
+  };
+
+  return (
+    <>
+      <div className="overflow-x-auto mt-5">
+        <table className="table table-zebra  border border-dashed border-neutral border-collapse">
+          <thead>
+            {tableInstance.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id} className="text-lg">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {tableInstance.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="join justify-center w-full mt-4">
+          <button className="join-item btn btn-active">1</button>
+          <button className="join-item btn ">2</button>
+          <button className="join-item btn ">3</button>
+          <button className="join-item btn ">4</button>
+        </div>
+      </div>
       {/* BEGIN: DELETE MODAL */}
       <dialog id="delete_modal" className="modal">
         <div className="modal-box">
@@ -174,9 +188,10 @@ const ScriptTable = () => {
               ✕
             </button>
           </form>
-          <h3 className="font-bold text-lg">Hello!</h3>
+          <h3 className="font-bold text-lg">Confirm Delete</h3>
           <p className="py-4">
-            Are you sure you want to delete {selectedForDelete?.script_name}?
+            Are you sure you want to delete `
+            <b>{selectedForDelete?.script_name}</b>`?
           </p>
           <div className="modal-action">
             <button
@@ -193,19 +208,27 @@ const ScriptTable = () => {
         </div>
       </dialog>
       {/* END: DELETE MODAL */}
-    </div>
+    </>
   );
 };
 
 const BrowseScripts = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetchScripts().then((data) => {
+      setData(data);
+    });
+  }, []);
+
   return (
     <>
       <div className="ml-64 mt-16 p-4">
         <h1>Browse Scripts</h1>
 
         <div className="overflow-x-auto">
-          <ScriptTable />
-          {/* scripts={scripts} /> */}
+          {/* <BrowseTable columns={columns} data={data} /> */}
+          <BrowseTable data={data} />
         </div>
       </div>
     </>

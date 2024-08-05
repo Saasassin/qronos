3ade8821dfcbc1e842522411024a94491c40d27a
Timothy Bess <timbessmail@gmail.com>
2024-08-04 21:01:59 -0400

    fix failover order in settings.py

--- backend/python/qronos/settings.py
+++ backend/python/qronos/settings.py
@@ -15,22 +15,23 @@ class Env(str, Enum):
 class Settings(BaseSettings):
     env: Env
     db_url: str
-
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
 def attempt_chdir(env_path: str):
-    if os.path.exists(env_path):
-        return env_path
-    if os.path.exists(os.path.basename(env_path)):
-        return os.path.basename(env_path)
     try:
-        os.chdir(os.environ["BUILD_WORKSPACE_DIRECTORY"] + os.path.dirname(env_path))
+        os.chdir(os.path.join(os.environ["BUILD_WORKSPACE_DIRECTORY"], os.path.dirname(env_path)))
         return os.path.basename(env_path)
     except Exception as e:
         logging.warning("Could not change directory to backend/python", exc_info=e)
         logging.warning(f"fallback to default path: {env_path}")
+
+    if os.path.exists(env_path):
         return env_path
+    if os.path.exists(os.path.basename(env_path)):
+        return os.path.basename(env_path)
+
+    return env_path
 
 
 if os.getenv("ENV") == "prod":
